b66a5ae40f3a7913ae4917a313790599cd449244
ShikharSingh98 <shikharsingh98@gmail.com>
2021-09-11 22:53:11 +0530

    feat: implemented modal functionality

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const openModal = document.querySelector('.open-modal');
+const modalContainer = document.querySelector('.modal-container');
+const closeModal = document.querySelector('.close-modal');
 
+openModal.addEventListener('click', function () {
+  modalContainer.style.display = 'block';
+});
+
+closeModal.addEventListener('click', function () {
+  modalContainer.style.display = 'none';
+});
+
+modalContainer.addEventListener('click', function (event) {
+  console.log(event.target);
+  if (event.target === modalContainer) {
+    modalContainer.style.display = 'none';
+  }
+});
